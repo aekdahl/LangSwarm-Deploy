cb3b04b9691febf3c973641ab6f3f2d7ff4b4615
aekdahl <alexander.ekdahl@gmail.com>
2025-01-09 18:15:44 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,68 @@
-module "langswarm_backend" {
-  source  = "./."
-  project_id = var.project_id
-  region     = var.region
+resource "google_artifact_registry_repository" "langswarm_repo" {
+  name         = "langswarm"
+  format       = "DOCKER"
+  location     = var.region
+  description  = "Artifact Registry for LangSwarm backend"
 }
 
-module "langswarm_frontend" {
-  source  = "./."
-  project_id = var.project_id
-  region     = var.region
+terraform {
+  backend "gcs" {
+    bucket  = "langswarm-terraform"
+    prefix  = "terraform/state"
+    project = var.project_id
+  }
+}
+
+resource "google_cloud_run_service" "langswarm_backend" {
+  name     = "langswarm-backend"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "europe-west1-docker.pkg.dev/${var.project_id}/langswarm/langswarm-service:latest"
+        ports {
+          container_port = 8080
+        }
+        env {
+          name  = "APP_PORT"
+          value = "8080"
+        }
+      }
+    }
+  }
+
+  autogenerate_revision_name = true
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service" "langswarm_frontend" {
+  name     = "langswarm-frontend"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "europe-west1-docker.pkg.dev/${var.project_id}/langswarm/langswarm-frontend:latest"
+        ports {
+          container_port = 80
+        }
+        env {
+          name  = "REACT_APP_BACKEND_URL"
+          value = google_cloud_run_service.langswarm_backend.status[0].url
+        }
+      }
+    }
+  }
+
+  autogenerate_revision_name = true
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  depends_on = [google_cloud_run_service.langswarm_backend]
 }
