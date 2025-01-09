471b1b6138052dca28a33a54d4470765bbb5d5c2
aekdahl <alexander.ekdahl@gmail.com>
2025-01-09 23:09:03 +0100

    Update main.tf

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -12,11 +12,18 @@ resource "google_project_service" "required_apis" {
   disable_on_destroy = false # Prevents Terraform from disabling APIs during resource destruction
 }
 
+data "google_artifact_registry_repository" "existing_repo" {
+  name     = "langswarm"
+  location = var.region
+  project  = var.project_id
+}
+
 resource "google_artifact_registry_repository" "langswarm_repo" {
-  repository_id    = "langswarm"
-  format           = "DOCKER"
-  location         = var.region
-  description      = "Artifact Registry for LangSwarm backend"
+  count    = length(data.google_artifact_registry_repository.existing_repo.name) > 0 ? 0 : 1
+  repository_id = "langswarm"
+  format        = "DOCKER"
+  location      = var.region
+  description   = "Artifact Registry for LangSwarm backend"
 }
 
 terraform {
