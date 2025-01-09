ac6eeb9b170bee88c86efa9e0b62e5b02ccf807d
aekdahl <alexander.ekdahl@gmail.com>
2025-01-09 22:00:20 +0100

    Update main.tf

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -1,1 +1,27 @@
+resource "google_project_service" "required_apis" {
+  for_each = toset([
+    "artifactregistry.googleapis.com",
+    "run.googleapis.com",
+    "storage.googleapis.com",
+    # "iamcredentials.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "logging.googleapis.com",
+    "compute.googleapis.com",
+  ])
+  service = each.value
+  disable_on_destroy = false # Prevents Terraform from disabling APIs during resource destruction
+}
 
+resource "google_artifact_registry_repository" "langswarm_repo" {
+  repository_id    = "langswarm"
+  format           = "DOCKER"
+  location         = var.region
+  description      = "Artifact Registry for LangSwarm backend"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "langswarm-terraform"
+    prefix  = "terraform/state"
+  }
+}
