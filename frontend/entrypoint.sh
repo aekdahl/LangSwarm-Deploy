36fb3a52e2ec03812d7abe13be2bd7ad969ae96e
aekdahl <alexander.ekdahl@gmail.com>
2025-01-10 10:04:39 +0100

    Create entrypoint.sh

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Generate the runtime config.js file with environment variables
+cat <<EOF > /usr/share/nginx/html/config.js
+window._env_ = {
+  REACT_APP_BACKEND_URL: "${REACT_APP_BACKEND_URL}"
+};
+EOF
+
+# Start Nginx
+exec "$@"
